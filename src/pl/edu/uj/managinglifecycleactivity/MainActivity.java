1bb586c21771b6e36202fe8d9abddd02107312f7
Bartłomiej Struzik <maccornik@gmail.com>
2012-11-13 00:51:32 +0100

    W MainActivity.java przykłąd sprawdzania wersji systemu
    Signed-off-by: Bartłomiej Struzik <maccornik@gmail.com>

--- src/pl/edu/uj/managinglifecycleactivity/MainActivity.java
+++ src/pl/edu/uj/managinglifecycleactivity/MainActivity.java
@@ -1,15 +1,31 @@
 package pl.edu.uj.managinglifecycleactivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	TextView textView;
 
-    @Override
+	@SuppressLint("NewApi")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        //Inicjalizacja textView
+        textView = (TextView) findViewById(R.id.text_message);
+        
+        //Sprawdzenie wersji Androida. Sprawdzamy czy mo¿emy u¿ywaæ ActionBarAPI
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+        	//Sprawdzenie czy ikona w ActionBar nie zachowuje siê jak guzik
+        	ActionBar actionBar = getActionBar();
+        	actionBar.setHomeButtonEnabled(false);
+        }
     }
 
     @Override
